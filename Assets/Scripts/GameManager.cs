cdc5e760499c0b925bd78fd9217039821ec73417
munim khaliq <97252132+munim-k@users.noreply.github.com>
2025-02-04 00:18:31 +0500

    day 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int Bullets;
+    public int Players;
+
+    public void Bullet1()
+    {
+        Bullets = 1;
+    }
+    public void Bullet2()
+    {
+        Bullets = 2;
+    }public void Bullet3()
+    {
+        Bullets = 3;
+    }public void Bullet4()
+    {
+        Bullets = 4;
+    }public void Bullet5()
+    {
+        Bullets = 5;
+    }public void Bullet6()
+    {
+        Bullets = 6;
+    }
+    public void Player1()
+    {
+        Players = 1;
+    }
+    public void Player2()
+    {
+        Players = 2;
+    }
+    public void Player3()
+    {
+        Players = 3;
+    }
+    public void Player4()
+    {
+        Players = 4;
+    }
+    public void Player5()
+    {
+        Players = 5;
+    }
+    public void Player6()
+    {
+        Players = 6;
+    }
+}
